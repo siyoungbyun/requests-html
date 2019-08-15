720437994bc37b0ee6e4f9909488b7aaf85166c7
Si Young Byun <44824967+siyoungbyun@users.noreply.github.com>
2019-08-15 16:06:05 -0500

    turned off signal handling

--- requests_html.py
+++ requests_html.py
@@ -779,7 +779,7 @@ class BaseSession(requests.Session):
     @property
     async def browser(self):
         if not hasattr(self, "_browser"):
-            self._browser = await pyppeteer.launch(ignoreHTTPSErrors=not(self.verify), headless=True, args=self.__browser_args)
+            self._browser = await pyppeteer.launch(ignoreHTTPSErrors=not(self.verify), headless=True, args=self.__browser_args, handleSIGINT=False, handleSIGTERM=False, handleSIGHUP=False)
 
         return self._browser
 
